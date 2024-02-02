31d6246ddf07530a3d70e2bb672dc5ff4218036b
Frank Hildebrandt <frank@hildebrandt.io>
2024-02-03 00:57:04 +0100

    more styling

--- src/FactionItem.tsx
+++ src/FactionItem.tsx
@@ -8,7 +8,7 @@ type Faction = {
 
 function FactionItem(faction: Faction): JSX.Element {
     return (
-        <div className="place-content-center">
+        <div className="flex justify-center">
             <div className="flex items-center bg-white shadow-lg rounded-lg overflow-hidden p-4 m-4 border w-9/12">
                 <div className="w-1/4">
                     <img src={faction.imageUrl} alt={faction.name}/>
