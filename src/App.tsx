8f9829f851a3e026eed45d5b257db0584dc03508
Frank Hildebrandt <frank@hildebrandt.io>
2024-02-06 12:38:53 +0100

    removed header

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,6 @@ function App() {
         <div className="App">
             <main>
                 <div className="flex flex-row">
-                    <div className="w-2/12 content-center">
-                        <div className="size-56">
-                        </div>
-                    </div>
-                    <div className="">
-                        <div className="text-3xl p-4 w-10/12">
-                            <img src="images/lsledger.png"/>
-                            <p className="mt-3"><i>Serving good and evil since 2k24</i></p>
-                        </div>
-                    </div>
                     <div className="fixed left-0 -bottom-3 opacity-70">
                         <img width="450" src="images/palm1.png"/>
                     </div>
@@ -23,13 +13,8 @@ function App() {
                         <img width="450" src="images/palm2.png"/>
                     </div>
                 </div>
-                <FactionView/>
-                <div className="flex justify-center">
-                    <div className="w-9/12">
-                        <div className="p-10 text-right text-neutral-900">
-                            (c) 2024 LSLedger Inc.
-                        </div>
-                    </div>
+                <div className="mt-2">
+                    <FactionView/>
                 </div>
             </main>
         </div>
